d21572f5abcfba697d1acd1bc9596c71a8f9e77a
Felix Nehren <felix@felixnehren.de>
2024-08-10 13:45:28 +0200

    add and improve api authorization

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const saveButton = document.getElementById('saveChanges');
   const resetButton = document.getElementById('resetButton');
   const apiForm = document.getElementById('apiForm');
-  const song_card = document.getElementById('song-card')
+  const song_card = document.getElementById('song-card');
 
   // Load saved settings from Chrome storage
-  chrome.storage.sync.get(['selectedOptions', 'vocals', 'autoplay', 'customTags', 'instruments', 'volume', 'api'], function(data) {
+  chrome.storage.sync.get(['selectedOptions', 'vocals', 'autoplay', 'customTags', 'instruments', 'volume', 'api', 'authToken'], function(data) {
     if (data.selectedOptions) {
       const options = JSON.parse(data.selectedOptions);
       for (const option of options) {
@@ -41,18 +41,21 @@ document.addEventListener('DOMContentLoaded', function() {
     if (data.api) {
       document.getElementById('api-endpoint').value = data.api;
     }
+    if (data.authToken) {
+      document.getElementById('auth-token').value = data.authToken;
+    }
   });
 
-function setSongCard() {
-  chrome.storage.local.get(['song'], function(data) {
-    console.log('CURRENT_SONG_DATA', data.song);
-    document.getElementById('cover').src = data.song.image_url;
-    document.getElementById('song-title').innerText = data.song.title;
-    document.getElementById('tags').innerText = `Tags: ${data.song.tags}`;
-    document.getElementById('lyricsContent').innerText = data.song.lyric;
-    song_card.classList.remove('d-none');
-  });
-}
+  function setSongCard() {
+    chrome.storage.local.get(['song'], function(data) {
+      console.log('CURRENT_SONG_DATA', data.song);
+      document.getElementById('cover').src = data.song.image_url;
+      document.getElementById('song-title').innerText = data.song.title;
+      document.getElementById('tags').innerText = `Tags: ${data.song.tags}`;
+      document.getElementById('lyricsContent').innerText = data.song.lyric;
+      song_card.classList.remove('d-none');
+    });
+  }
 
   // Set current song if playing
   chrome.storage.local.get(['song'], function(data) {
@@ -69,8 +72,9 @@ function setSongCard() {
     const customTags = document.getElementById('custom').value;
     const volume = document.getElementById('volume').value;
     const api = document.getElementById('api-endpoint').value;
+    const authToken = document.getElementById('auth-token').value;
 
-    // Save selections to chrome storage
+    // Save selections to Chrome storage
     chrome.storage.sync.set({
       selectedOptions: JSON.stringify(selectedOptions),
       instruments: JSON.stringify(selectedInstruments),
@@ -78,11 +82,28 @@ function setSongCard() {
       autoplay: autoplay,
       customTags: customTags,
       volume: volume,
-      api: api
+      api: api,
+      authToken: authToken
     }, function() {
       console.log('Settings saved.');
       const modal = bootstrap.Modal.getInstance(document.getElementById('settingsModal'));
       modal.hide();
+    });
+  }
+
+  // Open SettingsAccordion
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
     });
   }
 
@@ -104,21 +125,20 @@ function setSongCard() {
     document.getElementById('autoplay').checked = false;
     document.getElementById('volume').value = '50';
     document.getElementById('api-endpoint').value = 'http://localhost:3000/api/chatgpt';
+    document.getElementById('auth-token').value = '';
     chrome.storage.sync.clear();
 
     syncSettings();
   });
 
-
   // Set Spinning Button
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-
     const submitButton = document.getElementById('submitButton');
     const submitLabel = document.getElementById('submitLabel');
 
     if (message.generating === true) {
-    submitButton.setAttribute("disabled", true);
-    submitLabel.classList.add("spinner-border", "spinner-border-sm");
+      submitButton.setAttribute("disabled", true);
+      submitLabel.classList.add("spinner-border", "spinner-border-sm");
     }
 
     if (message.generating === false) {
@@ -140,7 +160,7 @@ function setSongCard() {
   document.getElementById('apiForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    await chrome.runtime.sendMessage({ submit: true});
+    await chrome.runtime.sendMessage({ submit: true });
   });
 
   document.getElementById('stopMusic').addEventListener('click', stopMusic);
--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 let audioQueue = [];
-chrome.storage.local.set({song: {}});
+chrome.storage.local.set({ song: {} });
 
 // Audio logic
 async function playSound(source = 'assets/sounds/default.mp3', volume = 0.1) {
@@ -10,54 +10,51 @@ async function playSound(source = 'assets/sounds/default.mp3', volume = 0.1) {
 
 async function playNextInQueue(submitted = false) {
     if (audioQueue.length > 0) {
-
         console.log('SUBMITTED', submitted);
-
         console.log('Play next in queue', audioQueue);
         const nextSound = audioQueue.shift();
         playSound(nextSound.item.audio_url, nextSound.volume);
-
         console.log('nextSong', nextSound.item);
-        chrome.storage.local.set({song: nextSound.item});
+        chrome.storage.local.set({ song: nextSound.item });
 
         if (submitted) {
             await chrome.runtime.sendMessage({ current_song: true });
-            console.log('setNextSong');    
+            console.log('setNextSong');
         }
     } else {
         console.log('NO SONG IN QUEUE', audioQueue);
-        chrome.storage.local.set({song: {}});
+        chrome.storage.local.set({ song: {} });
     }
 }
 
 function addToQueue(item, volume) {
-    console.log('addToQueue', item, volume)
+    console.log('addToQueue', item, volume);
     audioQueue.push({ item, volume });
 }
 
 let creating;
 async function setupOffscreenDocument(path) {
-  const offscreenUrl = chrome.runtime.getURL(path);
-  const existingContexts = await chrome.runtime.getContexts({
-    contextTypes: ['OFFSCREEN_DOCUMENT'],
-    documentUrls: [offscreenUrl]
-  });
-
-  if (existingContexts.length > 0) {
-    return;
-  }
-
-  if (creating) {
-    await creating;
-  } else {
-    creating = chrome.offscreen.createDocument({
-      url: path,
-      reasons: ['AUDIO_PLAYBACK'],
-      justification: 'Playing the received audio'
+    const offscreenUrl = chrome.runtime.getURL(path);
+    const existingContexts = await chrome.runtime.getContexts({
+        contextTypes: ['OFFSCREEN_DOCUMENT'],
+        documentUrls: [offscreenUrl]
     });
-    await creating;
-    creating = null;
-  }
+
+    if (existingContexts.length > 0) {
+        return;
+    }
+
+    if (creating) {
+        await creating;
+    } else {
+        creating = chrome.offscreen.createDocument({
+            url: path,
+            reasons: ['AUDIO_PLAYBACK'],
+            justification: 'Playing the received audio'
+        });
+        await creating;
+        creating = null;
+    }
 }
 
 async function handleFormSubmission(submitted = false) {
@@ -66,69 +63,86 @@ async function handleFormSubmission(submitted = false) {
     }
 
     chrome.storage.sync.get(
-        ['selectedOptions', 'vocals', 'autoplay', 'customTags', 'volume', 'instruments', 'api']
-        , function (syncedData) {
-        let { selectedOptions, vocals, autoplay, customTags, volume, instruments } = syncedData;
-        let api = syncedData.api || 'http://localhost:3000/api/chatgpt'
-        console.log('syncedData', syncedData);
-        console.log('api', api);
+        ['selectedOptions', 'vocals', 'autoplay', 'customTags', 'volume', 'instruments', 'api', 'authToken'],
+        function (syncedData) {
+            let { selectedOptions, vocals, autoplay, customTags, volume, instruments } = syncedData;
+            let api = syncedData.api || 'http://localhost:3000/api/chatgpt';
+            let authToken = syncedData.authToken || '';
+            console.log('syncedData', syncedData);
+            console.log('api', api);
 
-        selectedOptions = selectedOptions ? selectedOptions.concat(', ', instruments) : undefined;
+            selectedOptions = selectedOptions ? selectedOptions.concat(', ', instruments) : undefined;
 
-        chrome.storage.local.get(['content', 'title', 'url'], function (localData) {
-            const { content, title, url } = localData;
-            console.log('localData', localData);
+            chrome.storage.local.get(['content', 'title', 'url'], function (localData) {
+                const { content, title, url } = localData;
+                console.log('localData', localData);
 
-            const jsonData = {
-                multiselect: selectedOptions,
-                vocals: vocals,
-                tags: customTags,
-                htmlContent: content,
-                title: title
-            };
+                const jsonData = {
+                    multiselect: selectedOptions,
+                    vocals: vocals,
+                    tags: customTags,
+                    htmlContent: content,
+                    title: title
+                };
 
-            if ((submitted && !autoplay) || autoplay) {
-                fetch(api, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(jsonData),
-                })
-                .then(async response => {
-                    if (response.ok) {
-                        const jsonResponse = await response.json();
-                        jsonResponse.forEach(item => addToQueue(item, (parseFloat(volume) / 100)));
+                if ((submitted && !autoplay) || autoplay) {
+                    fetch(api, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'auth-token': authToken
+                        },
+                        body: JSON.stringify(jsonData),
+                    })
+                    .then(async response => {
+                        if (response.ok) {
+                            const jsonResponse = await response.json();
+                            jsonResponse.forEach(item => addToQueue(item, (parseFloat(volume) / 100)));
 
-                        playNextInQueue(submitted)
+                            playNextInQueue(submitted);
 
-                        if (submitted) {
-                            await chrome.runtime.sendMessage({ generating: false });
+                            if (submitted) {
+                                await chrome.runtime.sendMessage({ generating: false });
+                            }
+                        } else {
+                            if (submitted) {
+                                await chrome.runtime.sendMessage({ generating: false });
+                            }
+                            chrome.notifications.create(
+                                "APIError",
+                                {
+                                    type: "basic",
+                                    iconUrl: "error.png",
+                                    title: 'Failed to submit.',
+                                    message: 'Failed to submit the form. Check your API endpoint and auth token.'
+                                }
+                            );
+                            chrome.notifications.clear("APIError");
+
+                            throw new Error('Failed to submit the form. Possible issues with API endpoint or auth token.');
                         }
-                    } else {
+                    })
+                    .catch(async error => {
+                        console.error('Network error or API endpoint unreachable:', error);
+
                         if (submitted) {
                             await chrome.runtime.sendMessage({ generating: false });
                         }
                         chrome.notifications.create(
-                            "APIError",
+                            "NetworkError",
                             {
-                              type: "basic",
-                              iconUrl: "error.png",
-                              title: 'Failed to submit.',
-                              message: 'Failed to submit the form. Check your Api-Endpoint.'
+                                type: "basic",
+                                iconUrl: "error.png",
+                                title: 'Network Error',
+                                message: 'Could not connect to the API. Please check your internet connection or API endpoint.'
                             }
-                          );
-                        chrome.notifications.clear("APIError");
-
-                        throw new Error('Failed to submit the form.');
-                    }
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-            }
-        });
-    });
+                        );
+                        chrome.notifications.clear("NetworkError");
+                    });
+                }
+            });
+        }
+    );
 }
 
 // Add listener
@@ -148,6 +162,6 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
     }
 
     if (request.type === 'PLAYBACK_DONE') {
-      playNextInQueue();
-  }
+        playNextInQueue();
+    }
 });
